57aae8e166b6a84dc746671fc5c5f84aef3e8cef
adriqueirozz@gmail.com <adriqueirozz@gmail.com>
2023-11-07 14:32:11 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import openai
+
+app = Flask(__name__)
+
+openai.api_key = '' #adicione sua chave retirada na openai
+
+@app.route("/")
+
+def index():
+    return render_template("index.html")
+
+@app.route("/api", methods=["POST"])
+
+def api():
+    message = request.json.get("message")
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+
+    messages=[
+        {"role": "system", "content": "Você é um assistente de programação."},
+        {"role": "user", "content": message},
+    ]
+    )
+
+    if completion.choices[0].message!=None:
+            return completion.choices[0].message
+
+    else:
+            return 'Falha pra geração da resposta!'
+
+if __name__=='__main__':
+    app.run()
